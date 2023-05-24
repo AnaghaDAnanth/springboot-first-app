3f31dc6545035b23c4df6795732f6781d7c78e03
AnaghaDAnanth <anaghadananth106@gmail.com>
2023-05-24 16:02:41 +0530

    add list return,  path var and query param

--- src/main/java/com/springboot/first/app/StudentController.java
+++ src/main/java/com/springboot/first/app/StudentController.java
@@ -1,7 +1,11 @@
 package com.springboot.first.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -10,4 +14,35 @@ public class StudentController {
         return new Student("Anagha", "D Ananth");
     }
 
+    @GetMapping("/getStudents")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student("Pam", "Beesly"));
+        students.add(new Student("Jim", "Halpert"));
+        students.add(new Student("Dwight", "Schrute"));
+        return students;
+    }
+
+    @GetMapping("{firstName}/{lastName}")
+    // To retrieve a single resource
+    // http://localhost:8080/jim/halpert
+    // This simply returns a new object with the values passed in the URL.
+    // It does not retrieve the object from backend with same value.
+    public Student studentPathVariable(@PathVariable("firstName") String firstName,
+                                       @PathVariable("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+    @GetMapping("/student/query")
+    // To sort and filter
+    // http://localhost:8080/student?firstName=Jim&lastName=Halpert
+    // This simply returns a new object with the values passed in the URL.
+    // It does not retrieve the object from backend with same value.
+    public Student studentQueryParams(@RequestParam(name="firstName") String firstName,
+                                      @RequestParam(name="lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
 }
+
+
+
